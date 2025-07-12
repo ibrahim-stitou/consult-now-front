fe86e7160685bfc980e4ded0e6f50902cec4ea32
ibrahim stitou <149367384+ibrahim-stitou@users.noreply.github.com>
2025-07-12 22:55:18 +0100

    Update page.tsx

--- src/app/patient/consultation/mes-consultation/page.tsx
+++ src/app/patient/consultation/mes-consultation/page.tsx
@@ -6,13 +6,14 @@ import ConsultationsListing from '@/features/patient/consultations-listing';
 import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
 import { searchParamsCache, serialize } from '@/lib/searchparams';
 import { SearchParams } from 'nuqs/server';
-import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 export const metadata = {
   title: 'Mes consultations',
-  description: 'Liste de mes consultations et demandes'
+  description: 'Liste de mes consultations et demandes',
 };
 
 type PageProps = {
@@ -22,8 +23,8 @@ type PageProps = {
 export default async function Page(props: PageProps) {
   const searchParams = await props.searchParams;
   searchParamsCache.parse(searchParams);
-  const router = useRouter();
   const key = serialize({ ...searchParams });
+
   return (
     <PageContainer scrollable={false}>
       <div className="flex flex-1 flex-col space-y-4">
@@ -32,14 +33,17 @@ export default async function Page(props: PageProps) {
             title="Mes consultations"
             description="Consultez vos consultations et demandes de consultation"
           />
-          <Button
-            className="bg-green-600 hover:bg-green-700 text-white"
-            onClick={() => router.push('/patient/consultation/new')}
+          <Link
+            href="/patient/consultation/new"
+            className={cn(buttonVariants(), 'text-xs md:text-sm')}
           >
-            <Plus className="h-4 w-4 mr-2" /> Demander une consultation
-          </Button>
+            <Plus className="h-4 w-4 mr-2" />
+            Demander une consultation
+          </Link>
         </div>
+
         <Separator />
+
         <Suspense
           key={key}
           fallback={
